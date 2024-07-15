09e79528863dd3c72a4f491e898e52151d457a07
JoanaTav <157071427+JoanaTav@users.noreply.github.com>
2024-07-15 17:16:08 -0300

    Add files via upload

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <footer className="text-center bg-black text-bg-dark">
+      <p className="card-text py-3">
+      <i className="bi bi-c-circle"></i> Desenvolvido por Alura ⋅  JoanaTav 2024 | Projeto
+        fictício sem fins comerciais
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
